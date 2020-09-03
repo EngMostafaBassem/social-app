a9111b826b1eecae2238237555e9e9a9be8b3448
Mostafa <mostafa_basem@hotmail.com>
2020-09-03 19:25:34 +0200

    '932020724'

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express')
+const { env } = require('process')
 const app=express()
 
 app.get('/',(req,res)=>{
     res.end('Welcome from express')
 })
-app.listen(3001)
+app.listen(3001||env.port)
